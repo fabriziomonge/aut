47ee7a96f28fc8b8a8732b2654dd2d51a13cd877
Fabrizio Monge <37080465+fabriziomonge@users.noreply.github.com>
2021-01-06 17:59:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,21 +21,23 @@ def respond():
     
     name = str(name)
     psw = str(psw)
+    
+    try:
+        # Check if user sent a name at all
+        if not name:
+            response["ERROR"] = "no name found, please send a name."
+        # Check if the user entered a number not a name
+        elif str(name).isdigit():
+            response["ERROR"] = "name can't be numeric."
+        # Now the user entered a valid name
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    
-    elif data['PSW'][name]  == psw:
-        response["MESSAGE"] = "logged"
-    # Now the user entered a valid name
-    
-    
-    else:
+        elif data['PSW'][name]  == psw:
+            response["MESSAGE"] = "logged"
+        # Now the user entered a valid name
+
+        else:
+            response["MESSAGE"] = f"NOT LOGGED"
+    except:
         response["MESSAGE"] = f"NOT LOGGED"
 
     # Return the response in json format
